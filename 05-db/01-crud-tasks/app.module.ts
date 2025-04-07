cdd472d5068e8428f3c84fb48b768115779d3d7c
NickXL1138 <nickxl1138@gmail.com>
2025-04-07 23:31:39 +0700

    задание 5

--- 05-db/01-crud-tasks/app.module.ts
+++ 05-db/01-crud-tasks/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
+import { ConfigModule } from "@nestjs/config";
+import database from "./config/database";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [database] }),
+    TypeOrmModule.forRootAsync(database.asProvider()),
+    TasksModule,
+  ],
 })
 export class AppModule {}
--- 05-db/01-crud-tasks/tasks/tasks.controller.ts
+++ 05-db/01-crud-tasks/tasks/tasks.controller.ts
@@ -1,22 +1,47 @@
-import { Controller, Get, Post, Patch, Delete } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  ParseIntPipe,
+  Query,
+} from "@nestjs/common";
 import { TasksService } from "./tasks.service";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { UpdateTaskDto } from "./dto/update-task.dto";
 
 @Controller("tasks")
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
-  create() {}
+  create(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.create(createTaskDto);
+  }
 
   @Get()
-  findAll() {}
+  findAll(@Query("page") page?: number, @Query("limit") limit?: number) {
+    return this.tasksService.findAll(page, limit);
+  }
 
   @Get(":id")
-  findOne() {}
+  findOne(@Param("id", ParseIntPipe) id: string) {
+    return this.tasksService.findOne(+id);
+  }
 
   @Patch(":id")
-  update() {}
+  update(
+    @Param("id", ParseIntPipe) id: string,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ) {
+    return this.tasksService.update(+id, updateTaskDto);
+  }
 
   @Delete(":id")
-  remove() {}
+  remove(@Param("id", ParseIntPipe) id: string) {
+    return this.tasksService.remove(+id);
+  }
 }
