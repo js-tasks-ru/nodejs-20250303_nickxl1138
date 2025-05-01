318d1d1541b7807b50db766d2de400116d5bc679
NickXL1138 <nickxl1138@gmail.com>
2025-05-01 11:51:43 +0700

    задание 7

--- 07-authentication-authorization/01-google-auth/auth/jwt.guard.ts
+++ 07-authentication-authorization/01-google-auth/auth/jwt.guard.ts
@@ -12,7 +12,6 @@ export class JwtGuard extends AuthGuard("jwt") {
   canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
-      context.getClass(),
     ]);
 
     if (isPublic) {
