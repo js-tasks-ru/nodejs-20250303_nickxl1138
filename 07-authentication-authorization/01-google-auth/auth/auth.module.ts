4f2a2568ead3411125a84e37b19c2073cd05d456
NickXL1138 <nickxl1138@gmail.com>
2025-05-01 11:46:51 +0700

    задание 7

--- 07-authentication-authorization/01-google-auth/auth/auth.module.ts
+++ 07-authentication-authorization/01-google-auth/auth/auth.module.ts
@@ -2,10 +2,33 @@ import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { UsersModule } from "../users/users.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./passport/jwt.stategy";
+import { GoogleStrategy } from "./passport/google.strategy";
+import { ConfigService } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
+import { JwtGuard } from "./jwt.guard";
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get("JWT_SECRET"),
+        signOptions: { expiresIn: "10s" },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    JwtStrategy,
+    GoogleStrategy,
+    { provide: APP_GUARD, useClass: JwtGuard },
+  ],
 })
 export class AuthModule {}
